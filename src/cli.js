9eac4e323d5fa00cd2995225ec995c6578d7f15b
Illia Lebid <aidonline01@gmail.com>
2023-07-29 00:07:40 +0200

    Working prototype of main functionality of an application

---
+++ src/cli.js
@@ -0,0 +1,96 @@
+import readline from "readline";
+import store from './store';
+
+class Cli {
+  constructor() {
+    this.resolve = null;
+
+    readline.emitKeypressEvents(process.stdin);
+
+    if (process.stdin.isTTY)
+        process.stdin.setRawMode(true);
+
+    process.stdin.on('keypress', (chunk, key) => {
+      if(this.resolve) {
+        this.resolve(key);
+      }
+    });
+  }
+
+  async waitForKeypress() {
+    return new Promise((resolve) => {
+      this.resolve = resolve;
+    });
+  }
+
+  printInstructions() {
+    console.log('');
+    console.log('Press 1-9 numbers to choose an answer');
+    console.log('Press 0 to show progress');
+    console.log('Press ESC to exit');
+  }
+
+  exitIfEscIsPressed(pressedKey) {
+    if(pressedKey === 'escape') {
+      console.log('');
+      console.log('EXIT. Your game is saved');
+      process.exit();
+    }
+  }
+
+  async showProgressIfZeroIsPressed(pressedKey) {
+    if(pressedKey === '0') {
+      this.showProgress();
+
+      console.log('');
+      console.log('Press any key to continue...');
+      await this.waitForKeypress();
+
+      return true;
+    }
+
+    return false;
+  }
+
+  showQuestionAndAnswers(question, answers) {
+    console.log('');
+    console.log(`Translation for "${question.word}" is:`);
+    console.log(answers.join(' | '));
+  }
+
+  async showInstructionsIfIncorrectKeyIsPressed(chosenAnswer, pressedKey) {
+    if(!chosenAnswer && pressedKey !== '0') {
+        this.printInstructions();
+
+        console.log('');
+        console.log('Press any key to continue...');
+        await this.waitForKeypress();
+
+        return true;
+    }
+
+    return false;
+  }
+
+  showFeedback(chosenAnswer, correctAnswer) {
+    const colorRed = "\x1b[31m";
+    const colorGreen = "\x1b[32m";
+    const colorReset = "\x1b[0m";
+
+    console.log('');
+    console.log(chosenAnswer === correctAnswer ? colorGreen : colorRed, `Your answer: ${chosenAnswer}`);
+    console.log(colorGreen, `Correct answer: ${correctAnswer}`);
+    console.log(colorReset, '');
+
+  }
+
+  showProgress() {
+    const totalCount = store.getTotalCount();
+    const progress = store.getProgress();
+
+    console.log('');
+    console.log(`Progress words: ${progress}/${totalCount}`);
+  }
+}
+
+export default new Cli();
--- src/main.js
+++ src/main.js
@@ -1,14 +1,34 @@
 import store from './store.js';
+import cli from './cli.js';
 
-function initApplication() {
+async function initApplication() {
     let progress;
     let totalCount = store.getTotalCount();
+
+    cli.printInstructions();
+
     do {
         progress = store.getProgress();
         const question = store.getRandomWord();
         const answers = store.getRandomTranslations(question.word, question.translation);
 
-        store.answer(question.word, question.translation);
+        cli.showQuestionAndAnswers(question, answers);
+
+        const pressedKeyData = await cli.waitForKeypress();
+        const pressedKey = pressedKeyData.name;
+
+        const chosenAnswer = answers[+pressedKey - 1];
+
+        cli.exitIfEscIsPressed(pressedKey);
+        const progressIsShown = await cli.showProgressIfZeroIsPressed(pressedKey);
+        const instructionIsShown = await cli.showInstructionsIfIncorrectKeyIsPressed(chosenAnswer, pressedKey);
+
+        if(instructionIsShown) continue;
+        if(progressIsShown) continue;
+
+        store.answer(question.word, chosenAnswer);
+        
+        cli.showFeedback(chosenAnswer, question.translation);
     } while(progress < totalCount - 1);
 
     console.log('GAME IS WON!!!!')
