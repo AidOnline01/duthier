a6f3527b212cd5043be9c4981c7ce7626ba96523
Illia Lebid <aidonline01@gmail.com>
2023-07-28 23:07:56 +0200

    Add answering functionality

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import store from './store.js';
 
 function initApplication() {
-    const question = store.getRandomWord();
-    const answers = store.getRandomTranslations(question.word, question.translation);
-    console.log(question, answers);
+    let progress;
+    let totalCount = store.getTotalCount();
+    do {
+        progress = store.getProgress();
+        const question = store.getRandomWord();
+        const answers = store.getRandomTranslations(question.word, question.translation);
+
+        store.answer(question.word, question.translation);
+    } while(progress < totalCount - 1);
+
+    console.log('GAME IS WON!!!!')
 }
 
 initApplication();
--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
-import nl from '@/dictionaries/nl.json';
-import en from '@/dictionaries/en.json';
+import nl from '@/dictionaries-test/nl.json';
+import en from '@/dictionaries-test/en.json';
 
 class Store {
   constructor() {
@@ -35,6 +35,7 @@ class Store {
     const word = this.state.activeWords[randomIndex];
 
     const wordData = this.state.wordsProgress[word];
+
     return {word, hits: wordData.hits, translation: wordData.translation};
   }
 
@@ -43,16 +44,41 @@ class Store {
     const chosenWords = [];
 
     for(let i = 0; i < 5; i++) {
-      const randomIndex = Math.floor(Math.random() * en.words.length);
+      const randomIndex = Math.floor(Math.random() * wordsPool.length);
 
       const chosenWord = wordsPool.splice(randomIndex, 1)[0];
-      
+
       if(chosenWord !== word) chosenWords.push(chosenWord);
     }
 
     chosenWords.push(translation);
-    
+
     return chosenWords.sort(() => 0.5 - Math.random());
+  }
+
+  getTotalCount() {
+    return nl.words.length;
+  }
+
+  getProgress() {
+    return nl.words.length - this.state.activeWords.length;
+  }
+
+  answer(word, answer) {
+    const wordData = this.state.wordsProgress[word];
+
+    if(wordData.translation !== answer) return;
+
+    wordData.hits += 1;
+
+    if(wordData.hits >= 5) {
+      this.deleteWord(word);
+    }
+  }
+
+  deleteWord(word) {
+    delete this.state.wordsProgress[word];
+    this.state.activeWords = this.state.activeWords.filter(item => item !== word);
   }
 }
 
